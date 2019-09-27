bb0018f339952cb725bdc1a75e5ad1257c175b37
I322118 <ravi.suresh.mashru@sap.com>
2019-09-27 14:44:55 +0530

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ravi Mashru`,
+    description: `Ravi Mashru's home page on the internet - personal portfolio, blog and notes.`,
+    author: `@mashruravi`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Ravi Mashru's Home Page`,
+        short_name: `Ravi Mashru`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#000000`,
+        theme_color: `#FFFFFF`,
+        display: `standalone`,
+        icon: `src/images/rm-hires.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
