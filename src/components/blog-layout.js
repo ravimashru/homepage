3f76f863f0e09458a98569a58d0b45db823e7888
Ravi Mashru <mashru.ravi@gmail.com>
2020-02-09 16:49:01 +0530

    Show name of blog post in title
    Fixes #3

--- src/components/blog-layout.js
+++ src/components/blog-layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../components/layout';
+import SEO from './seo';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { graphql } from 'gatsby';
 
 export default function BlogLayout({ data: { mdx } }) {
   return (
     <Layout>
+      <SEO title={`${mdx.frontmatter.title} - Blog`} />
       <div className="blog-content">
         <h1>{mdx.frontmatter.title}</h1>
         <div className="blog-body">
