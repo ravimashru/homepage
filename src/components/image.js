b1701e032b3568154a1131a3433337fce7906463
Ravi Suresh Mashru <ravi.suresh.mashru@sap.com>
2020-06-17 08:14:10 +0530

    use Image component to display images

--- src/components/image.js
+++ src/components/image.js
@@ -16,17 +16,22 @@ import Img from "gatsby-image"
 const Image = (props) => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
+      allFile {
+          nodes {
+            relativePath
+            childImageSharp {
+              fluid(maxWidth: 1200) {
+                ...GatsbyImageSharpFluid
+              }
+            }
         }
       }
     }
-  `)
+  `);
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  const match = data.allFile.nodes.find(e => e.relativePath === props.name);
+
+  return <Img fluid={match.childImageSharp.fluid} />
 }
 
 export default Image
