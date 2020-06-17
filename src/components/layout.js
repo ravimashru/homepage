248d3dc107995d1794d25414030f4db85e2a2fd0
Ravi Suresh Mashru <ravi.suresh.mashru@sap.com>
2020-06-17 08:10:20 +0530

    add z-index to header

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,7 +16,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <div style={{ position: 'fixed', top: 0, left: 0, right: 0 }}>
+      <div style={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 1 }}>
         <Header />
       </div>
       <main
