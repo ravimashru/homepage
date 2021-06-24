5e1685efd43ee16cf45787b6ce72dc36e23a72f3
Ravi Suresh Mashru <ravi.suresh.mashru@sap.com>
2021-06-24 22:28:35 +0530

    fix header alignment

--- src/components/profile-header.js
+++ src/components/profile-header.js
@@ -3,48 +3,50 @@ import me from '../images/me.png';
 
 export default function ProfileHeader() {
   return (
-    <div className="prf-hdr-ctr">
-      <div className="prf-hdr-img">
-        <img src={me} alt="me" style={{ objectFit: 'contain', width: '180px' }} />
-      </div>
-      <div className="prf-hdr-intro">
-        <div>
-          Hello! I am <strong>Ravi Mashru</strong>.
+    <div className="prf-hdr-grid-ctr">
+      <div className="prf-hdr-ctr">
+        <div className="prf-hdr-img">
+          <img src={me} alt="me" style={{ objectFit: 'contain', width: '180px' }} />
         </div>
-        <div>
-          I'm a full-stack web developer.
+        <div className="prf-hdr-intro">
+          <div>
+            Hello! I am <strong>Ravi Mashru</strong>.
+          </div>
+          <div>
+            I'm a full-stack web developer.
+          </div>
         </div>
-      </div>
-      <div className="prf-hdr-social">
-        <div className="social-icon si si-linkedin">
-          <a
-            href="https://www.linkedin.com/in/ravi-mashru-4a7ba956/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            i
-          </a>
-        </div>
-        <div className="social-icon si si-twitter">
-          <a
-            href="https://twitter.com/ravimashru"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            l
-          </a>
-        </div>
-        <div className="social-icon sip si-mail">
-          <a href="mailto:me@ravimashru.dev">m</a>
-        </div>
-        <div className="social-icon sip si-github">
-          <a
-            href="https://github.com/ravimashru"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            z
-          </a>
+        <div className="prf-hdr-social">
+          <div className="social-icon si si-linkedin">
+            <a
+              href="https://www.linkedin.com/in/ravi-mashru-4a7ba956/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              i
+            </a>
+          </div>
+          <div className="social-icon si si-twitter">
+            <a
+              href="https://twitter.com/ravimashru"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              l
+            </a>
+          </div>
+          <div className="social-icon sip si-mail">
+            <a href="mailto:me@ravimashru.dev">m</a>
+          </div>
+          <div className="social-icon sip si-github">
+            <a
+              href="https://github.com/ravimashru"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              z
+            </a>
+          </div>
         </div>
       </div>
     </div>
